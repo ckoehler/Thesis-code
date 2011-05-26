1e65579f975ce347496c56c69c2ca5ed861e2c0c
Christoph <christoph@zerodeviation.net>
2011-05-26 15:13:28 -0500

    add some more files.

---
+++ impulse_response_examples.m
@@ -0,0 +1,29 @@
+clear all;
+close all;
+v_max = 100;
+carrier = 9.55e9;
+
+
+tau = 200e-6;
+amp = ones(1,13);
+fs = 2e6;
+f_points = 100;
+impulse_response = [];
+[clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
+[delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
+t_str = sprintf('Pulse w/o IR distortion ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
+fig = plotaf(t_str, delay,v,the_af);
+print(fig, '-depsc', '-tiff', '-r300', '../thesis/figures/pulsewoir-200us.eps');
+
+impulse_response = [0 .1 .2 .4 .5 .8  1 .8 .5 .4 .2 .1 0];
+[clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
+[delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
+t_str = sprintf('Pulse w/ IR distortion ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
+fig = plotaf(t_str, delay,v,the_af);
+print(fig, '-depsc', '-tiff', '-r300', '../thesis/figures/pulsewir-200us.eps');
+
+
+fig = figure;
+plot(impulse_response);
+title('Transfer function');
+print(fig, '-depsc', '-tiff', '-r300', '../thesis/figures/tf1.eps');
---
+++ plotaf.m
@@ -0,0 +1,17 @@
+function fig = plotaf(t_str, delay,v,af)
+  fig = figure;
+  % TODO: need to adjust the delay. If we have an IR signal, the delay will be longer, tau*(2*m-1).
+  surface(delay, v, af);
+  %surface(AF);
+  view(-40,50);
+  title(t_str,'FontSize',12);
+  xlabel('Range delay in m    ','FontSize',12);
+  ylabel('Radial velocity in m/s     ','FontSize',12);
+  zlabel('Normalized magnitude     ','FontSize',12);
+  axis([-inf inf -inf inf 0 1]);
+  shading flat;
+  %shading faceted;
+  %cmrow = [41	143	165] ./ 255;
+  %cm=repmat(cmrow, [64 1]); 	
+  %colormap(cm);
+end
