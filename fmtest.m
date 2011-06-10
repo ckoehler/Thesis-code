47f8f1faa794152384c6e58fe9cf2e138982a737
Christoph <christoph@zerodeviation.net>
2011-06-10 15:02:02 -0500

    More playtime.

--- fmtest.m
+++ fmtest.m
@@ -4,7 +4,7 @@ a = (linspace(0,10,100));
 
 for i=1:length(a)
   for j=1:length(b)
-    plot(generate_arbitrary_fm(1,100,1, b(j),a(i)))
+    plot(generate_arbitrary_fm(1,100,5, b(j),a(i)))
     s = sprintf('%i:%i',i,j);
     title(s);
     pause(.1)
--- generate_arbitrary_fm.m
+++ generate_arbitrary_fm.m
@@ -1,7 +1,7 @@
 function f=generate_arbitrary_fm(tau, fs, a,b, c)
   N = tau*fs;
   n = linspace(-1,1,N);
-  x = c.*n.^5 + a.*n.^3 + b.*n;
+  x = c.*n.^7 + a.*n.^3 + b.*n;
   x = x./(max(abs(x)));
   f = x;
 end
