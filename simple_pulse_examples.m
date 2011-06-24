b69912440baad5d5eded0c09bbbe4a664d1d22bc
Christoph <christoph@zerodeviation.net>
2011-06-24 11:52:15 -0500

    Make pulse examples work with new makesignal.

--- simple_pulse_examples.m
+++ simple_pulse_examples.m
@@ -5,12 +5,13 @@ carrier = 9.55e9;
 series_name = 'pulse';
 
 
-amp = ones(1,13);
 f_points = 100;
 impulse_response = [];
 
 tau = 1e-6;
 fs = 2e8;
+N = tau*fs;
+amp = ones(1,N);
 [clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
 [delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
 t_str = sprintf('Simple pulse ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
@@ -19,7 +20,9 @@ filename = sprintf('../thesis/figures/%s-%ius.png', series_name,tau*1e6);
 print(fig, '-dpng', '-r300', filename);
 
 tau = 200e-6;
-fs = 1e6;
+fs = 5e4;
+N = tau*fs;
+amp = ones(1,N);
 [clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
 [delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
 t_str = sprintf('Simple pulse ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
