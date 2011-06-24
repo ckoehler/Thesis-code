f3a3cb69e360e53a3bff5137e1280a575dec6ed7
Christoph <christoph@zerodeviation.net>
2011-06-24 14:39:28 -0500

    Fix Doppler test.

--- dopplertest.m
+++ dopplertest.m
@@ -8,28 +8,28 @@ carrier = 9.55e9;
 
 
 lambda = 3e8/9.55e9;
-v=1000;
+v=50;
 amp = ones(1,200);
 f_points = 100;
 impulse_response = [];
 
 tau = 1e-6;
-fs = 2e8;
+fs = 2e7;
 [s foo ntau] = makesignal(amp, [], [], impulse_response, tau, fs);
 l = length(s);
-dshift = exp(1i*2*pi/lambda*linspace(0,v,l).*(0:l-1)/fs);
-s = s.*dshift;
-acf1 = abs(conv(s,fliplr(conj(s)),'same'));
+dshift = exp(1i*2*pi/lambda.*v.*(0:l-1)/fs);
+sd = s.*dshift;
+acf1 = abs(conv(s,fliplr(conj(sd)),'same'));
 
 
 tau2 = 200e-6;
-fs = 1e7;
+fs = 2e7;
 [s2 foo ntau] = makesignal(amp, [], [], impulse_response, tau2, fs);
 l = length(s2);
-dshift = exp(1i*2*pi/lambda*linspace(0,v,l).*(0:l-1)/fs);
-s2 = s2.*dshift;
+dshift = exp(1i*2*pi/lambda.*v.*(0:l-1)/fs);
+sd2 = s2.*dshift;
 
-acf2 = abs(conv(s2,fliplr(conj(s2)),'same'));
+acf2 = abs(conv(s2,fliplr(conj(sd2)),'same'));
 
 
 subplot(2,1,1);
