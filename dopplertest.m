1b7a95a9497d690b09b476f9139d341e3f27b6b8
Christoph <christoph@zerodeviation.net>
2011-06-24 16:23:58 -0500

    Use xcorr and make sure that the signal length is right, and not constant.

--- dopplertest.m
+++ dopplertest.m
@@ -1,35 +1,38 @@
 close all;
 clear all;
 
-
-
-v_max = 100;
 carrier = 9.55e9;
 
 
-lambda = 3e8/9.55e9;
+lambda = 3e8/carrier;
 v=50;
-amp = ones(1,200);
 f_points = 100;
 impulse_response = [];
 
 tau = 1e-6;
-fs = 2e7;
+fs = 2e8;
+N=tau*fs;
+amp = ones(1,N);
 [s foo ntau] = makesignal(amp, [], [], impulse_response, tau, fs);
 l = length(s);
 dshift = exp(1i*2*pi/lambda.*v.*(0:l-1)/fs);
 sd = s.*dshift;
-acf1 = abs(conv(s,fliplr(conj(sd)),'same'));
+%acf1 = abs(conv(s,fliplr(conj(sd)),'same'));
+acf1 = abs(xcorr(s,sd));
 
 
 tau2 = 200e-6;
-fs = 2e7;
+fs = 2e5;
+N=tau2*fs;
+%N = 200;
+amp = ones(1,N);
 [s2 foo ntau] = makesignal(amp, [], [], impulse_response, tau2, fs);
 l = length(s2);
 dshift = exp(1i*2*pi/lambda.*v.*(0:l-1)/fs);
 sd2 = s2.*dshift;
 
-acf2 = abs(conv(s2,fliplr(conj(sd2)),'same'));
+%acf2 = abs(conv(s2,fliplr(conj(sd2)),'same'));
+acf2 = abs(xcorr(s2,sd2));
 
 
 subplot(2,1,1);
