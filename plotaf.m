408750cff8a0bd009812aeca08fef47f34f9d68d
Christoph <christoph@zerodeviation.net>
2011-06-24 16:23:14 -0500

    Use argument to tell the plotaf function to be in db or linear.

--- plotaf.m
+++ plotaf.m
@@ -1,22 +1,33 @@
-function fig = plotaf(t_str, delay,v,af)
+function fig = plotaf(t_str, delay,v,af, uselin)
   fig = figure;
+
+  if nargin==4
+    db = true;
+  else
+    db = ~uselin;
+  end
 
   l = size(af);
   s = length(delay);
 
-  % if for some reason the dimensions don't quite match up, slice the af to make 
-  % it fit.
-  if l(2) ~= s
-    af = af(:,1:s);
+  if db
+    af=20*log10(af);
+    af(af<-100) = NaN;
   end
-  af=20*log10(af);
-  af(af<-100) = NaN;
+
   surface(delay, v, af);
+  %surface(af);
   view(-40,50);
   title(t_str,'FontSize',12);
   xlabel('Range delay in m    ','FontSize',12);
   ylabel('Radial velocity in m/s     ','FontSize',12);
-  zlabel('Normalized magnitude in dB    ','FontSize',12);
+
+  if db
+    zlabel('Normalized magnitude in dB    ','FontSize',12);
+  else
+    zlabel('Normalized magnitude ','FontSize',12);
+  end
+
   %axis([-inf inf -inf inf -inf inf]);
   shading flat;
   %shading faceted;
