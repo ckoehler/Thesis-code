5d87f80395d2e35119a54a1f9c6b6aad0089a24c
Christoph <christoph@zerodeviation.net>
2011-06-24 16:23:41 -0500

    Make adjustments to work with the new af and plotaf functions.

--- simple_pulse_examples.m
+++ simple_pulse_examples.m
@@ -1,6 +1,6 @@
 clear all;
 close all;
-v_max = 100;
+v_max = 50;
 carrier = 9.55e9;
 series_name = 'pulse';
 
@@ -13,19 +13,19 @@ fs = 2e8;
 N = tau*fs;
 amp = ones(1,N);
 [clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
-[delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
+[delay v the_af] = af(signal, clean_signal, new_tau, fs, v_max, f_points, carrier);
 t_str = sprintf('Simple pulse ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
-fig = plotaf(t_str, delay,v,the_af);
+fig = plotaf(t_str, delay,v,the_af,true);
 filename = sprintf('../thesis/figures/%s-%ius.png', series_name,tau*1e6);
 print(fig, '-dpng', '-r300', filename);
 
 tau = 200e-6;
-fs = 5e4;
+fs = 2e5;
 N = tau*fs;
 amp = ones(1,N);
 [clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
-[delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
+[delay v the_af] = af(signal, clean_signal, new_tau, fs, v_max, f_points, carrier);
 t_str = sprintf('Simple pulse ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
-fig = plotaf(t_str, delay,v,the_af);
+fig = plotaf(t_str, delay,v,the_af, true);
 filename = sprintf('../thesis/figures/%s-%ius.png', series_name,tau*1e6);
 print(fig, '-dpng', '-r300', filename);
