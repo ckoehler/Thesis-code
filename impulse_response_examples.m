5f0853dacc70255017aab1feb23dbc5bb5f38c3f
Christoph <christoph@zerodeviation.net>
2011-07-01 15:19:39 -0500

    Fix up the IR examples.

--- impulse_response_examples.m
+++ impulse_response_examples.m
@@ -1,29 +1,32 @@
 clear all;
 close all;
-v_max = 100;
+v_max = 50;
 carrier = 9.55e9;
 
-
-tau = 200e-6;
-amp = ones(1,13);
-fs = 1e5;
+tau = 15e-6;
+fs = 2e8;
+N = tau*fs;
+amp = ones(1,N);
 f_points = 100;
 impulse_response = [];
 [clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
-[delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
+[delay v the_af] = af(signal, clean_signal, new_tau, fs, v_max, f_points, carrier);
 t_str = sprintf('Pulse w/o IR distortion ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
 fig = plotaf(t_str, delay,v,the_af);
-print(fig, '-dpng', '-r300', '../thesis/figures/pulsewoir-200us.png');
+print(fig, '-dpng', '-r300', '../thesis/figures/pulsewoir-15us.png');
 
-impulse_response = [0 .1 .2 .4 .5 .8  1 .8 .5 .4 .2 .1 0];
+%impulse_response = [0 .1 .2 .4 .5 .8  1 .8 .5 .4 .2 .1 0];
+domain = linspace(0,5,N);
+impulse_response = raylpdf(domain, 1);
+impulse_response = impulse_response/max(impulse_response);
 [clean_signal signal new_tau] = makesignal(amp, [], [], impulse_response, tau, fs);
-[delay v the_af] = af(signal, clean_signal, new_tau, v_max, f_points, carrier);
+[delay v the_af] = af(signal, clean_signal, new_tau, fs, v_max, f_points, carrier);
 t_str = sprintf('Pulse w/ IR distortion ( \\tau=%3.3e s, f=%1.2f GHz)      ', tau, carrier./1e9);
 fig = plotaf(t_str, delay,v,the_af);
-print(fig, '-dpng', '-r300', '../thesis/figures/pulsewir-200us.png');
+print(fig, '-dpng', '-r300', '../thesis/figures/pulsewir-15us.png');
 
 
 fig = figure;
 plot(impulse_response);
-title('Transfer function');
+title('Impulse response');
 print(fig, '-dpng', '-r300', '../thesis/figures/tf1.png');
