823f9a441b891961fe1e50803de67ff210d8a3ea
Christoph <christoph@zerodeviation.net>
2011-07-27 11:11:32 -0500

    Fix some bugs and adjust example file.

--- af.m
+++ af.m
@@ -7,13 +7,18 @@ function [delay v af] = af(impulse_response, signal, tau, fs, v_max, f_points, c
   % clean_signal = if different from signal, this is the signal we use to create the time-
   %                 shifted version, or u'(t-tau) above.
   % tau = signal length
+
+  m = length(signal);
   
   % if no signal is given, assume that all we have is a clean signal
   % and use that.
   if isempty(impulse_response)
     ir=false;
+    m_ir = m;
   else
     ir=true;
+    m_ir = m;
+    tau = 2*tau;
   end
 
   if nargin < 8
@@ -26,7 +31,6 @@ function [delay v af] = af(impulse_response, signal, tau, fs, v_max, f_points, c
   % wavelength
   lam = c/carrier;
 
-  m = length(signal);
   %m_clean = length(clean_signal);
 
   af = [];
@@ -41,6 +45,7 @@ function [delay v af] = af(impulse_response, signal, tau, fs, v_max, f_points, c
     f = linspace(0,f_max, f_points);
   end
 
+
   %f = [1.5917e3 1.5917e3];
   for i=1:length(f)
     dshift = exp(1i*2*pi.*f(i).*(0:m-1)/fs);
@@ -48,7 +53,7 @@ function [delay v af] = af(impulse_response, signal, tau, fs, v_max, f_points, c
 
     if ir
       shifted_s = conv(shifted_s, impulse_response); 
-      tau = 2*tau;
+      m_ir = length(shifted_s);
     end
 
     af(i,:) = abs(xcorr(signal, shifted_s));
@@ -58,7 +63,7 @@ function [delay v af] = af(impulse_response, signal, tau, fs, v_max, f_points, c
   af = af./max(max(af));
 
   % compute delay
-  t = linspace(0, tau, m);
+  t = linspace(0, tau, m_ir);
   delay = [-fliplr(t) t(2:end)] * c / 2;
 
   % convert doppler frequency to velocity
